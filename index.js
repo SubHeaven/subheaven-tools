b6f527227da95e666b3cd4be8c98d77fc3f045be
SubHeaven <subheaven.paulo@gmail.com>
2021-05-20 14:33:55 -0300

    Atualização do pacote para o padrão do Error Lens

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-Array.prototype.forEachAsync = async function(fn) {
-    let i = 0;
-    for (let t of this) {
-        await fn(t, i);
-        i++;
-    }
-}
-
-Array.prototype.forEachAsyncParallel = async function(fn) {
-    await Promise.all(this.map(fn));
-}
-
-Number.prototype.toByteString = async function(si = false, dp = 1) {
-    let bytes = this;
-    const thresh = si ? 1000 : 1024;
-
-    if (Math.abs(bytes) < thresh) {
-        return bytes + ' B';
+export default await (async() => {
+    Array.prototype.forEachAsync = async function(fn) {
+        let i = 0;
+        for (let t of this) {
+            await fn(t, i);
+            i++;
+        }
     }
 
-    const units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
-    let u = -1;
-    const r = 10 ** dp;
+    Array.prototype.forEachAsyncParallel = async function(fn) {
+        await Promise.all(this.map(fn));
+    }
 
-    do {
-        bytes /= thresh;
-        ++u;
-    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+    Number.prototype.toByteString = async function(si = false, dp = 1) {
+        let bytes = this;
+        const thresh = si ? 1000 : 1024;
+
+        if (Math.abs(bytes) < thresh) {
+            return bytes + ' B';
+        }
+
+        const units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+        let u = -1;
+        const r = 10 ** dp;
+
+        do {
+            bytes /= thresh;
+            ++u;
+        } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
 
 
-    return bytes.toFixed(dp) + ' ' + units[u];
-}
+        return bytes.toFixed(dp) + ' ' + units[u];
+    }
 
-exports.hello = null;
+    const index = {
+        init: async() => {}
+    }
+
+    return index;
+})();
